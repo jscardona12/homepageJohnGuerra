0f4b69d32ae99b245c85f341280aadcf1581c144
John Alexis Guerra Gómez <john.guerra@gmail.com>
2016-12-12 05:31:26 -0500

    Adding projects page

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-rsync -avz --progress -e "ssh -i /Users/aguerra/documentos/dutoViz/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
+rsync -av -e "ssh -i /Users/aguerra/documentos/dutoViz/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
