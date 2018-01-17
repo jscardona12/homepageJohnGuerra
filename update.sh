7e013f26870ef9c07dc4b504eb657fa23efb680d
John Alexis Guerra Gómez <john.guerra@gmail.com>
2018-01-17 12:14:26 -0500

    Updating my photo and netviz pages

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/documentos/dutoViz/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
+rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
