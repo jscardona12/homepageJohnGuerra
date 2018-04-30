2d6a4b48533fb2806e4c32590e85c567b0647e7d
John Alexis Guerra Gómez <john.guerra@gmail.com>
2018-04-30 11:17:19 -0500

    Updating cv

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@
 
 
 rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/dutoVizNew.pem" * ubuntu@johnguerra.co:/var/www/johnguerra.co
+
+rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/tweetometro2.pem" * ubuntu@18.231.179.33:/var/www/johnguerra.co
+rsync -avzgu --delete --partial -e "ssh -i /Users/aguerra/Dropbox/tweetometro_paz.pem" * ubuntu@34.230.24.73:/var/www/johnguerra.co
